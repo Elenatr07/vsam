ae5f9113b35329a145c78173d5da8cef3eab950b
Elenatr07 <e.turkina2010@yandex.ru>
2024-05-09 11:41:31 +1000

    3

--- scripts/show_more.js
+++ scripts/show_more.js
@@ -1,7 +1,53 @@
 function show_more_security_consalting(){
     let btnText = document.getElementById("load_btn_block_one");
-    let els = document.getElementsByClassName("step");
+    let els = document.getElementsByClassName("step1");
    var arrow = document.getElementById("load_btn_block_one");
+   let bottom = document.getElementById("bottom_menu");
+  bottom.classList.toggle('bottom_menu');
+  
+    arrow.classList.toggle('up');
+   
+    
+    for (var i = 0, el; el = els[i]; i++){
+      if (getComputedStyle(el).display == 'none') {
+        el.style.display = 'block';
+        
+        
+      } else {
+       el.style.display = "none";
+       
+      }
+   //console.log(els);
+  }
+}
+function show_more_network_consalting(){
+    let btnText = document.getElementById("load_btn_block_two");
+    let els = document.getElementsByClassName("step2");
+   var arrow = document.getElementById("load_btn_block_two");
+   let bottom = document.getElementById("bottom_menu1");
+   bottom.classList.toggle('bottom_menu1');
+   
+   arrow.classList.toggle('up');
+   
+    
+    for (var i = 0, el; el = els[i]; i++){
+      if (getComputedStyle(el).display == 'none') {
+        el.style.display = 'block';
+        
+        
+      } else {
+       el.style.display = "none";
+       
+      }
+   //console.log(els);
+  }
+}
+function show_more_infrastructure_consalting(){
+    let btnText = document.getElementById("load_btn_block_three");
+    let els = document.getElementsByClassName("step3");
+   var arrow = document.getElementById("load_btn_block_three");
+   let bottom = document.getElementById("bottom_menu2");
+   bottom.classList.toggle('bottom_menu');
    
    arrow.classList.toggle('up');
    
