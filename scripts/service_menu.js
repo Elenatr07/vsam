2076cd23554b8dcc7851b2922b704f7e5372ee6d
Elenatr07 <e.turkina2010@yandex.ru>
2024-05-10 01:55:07 +1000

    8

--- scripts/service_menu.js
+++ scripts/service_menu.js
@@ -21,3 +21,27 @@ function rechangeItem(){
        document.getElementById("services_menu_item1_p").classList.remove("style")
   
 }
+
+function changeItem1(){
+    if ( document.getElementById("services_menu_item2").classList.contains('services_menu_item2') ){
+       document.getElementById("services_menu_item2").classList.add('service_menu_hover2'); 
+       document.getElementById("services_menu_item1").classList.add('service_menu_hover1'); 
+       document.getElementById("services_menu_item1_p").style.color = 'rgba(248, 248, 255, 1)';
+       document.getElementById("services_menu_item2_p").style.color = 'rgba(0, 0, 0, 1)';
+      document.getElementById("services_menu_item1_p").classList.toggle("style")
+      
+       
+    }
+
+    
+
+}
+function rechangeItem1(){
+   
+       document.getElementById("services_menu_item2").classList.remove('service_menu_hover2'); 
+       document.getElementById("services_menu_item1").classList.remove('service_menu_hover1');
+       document.getElementById("services_menu_item1_p").style.color = 'rgba(0, 0, 0, 1)';
+       document.getElementById("services_menu_item2_p").style.color = 'rgba(255, 255, 255, 1)';
+       document.getElementById("services_menu_item1_p").classList.remove("style")
+  
+}
