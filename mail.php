bbb22ec80a5ab1cb457f3829e7f2d7fcbfc9ec9f
Elenatr07 <e.turkina2010@yandex.ru>
2024-05-12 04:58:46 +1000

    20

--- mail.php
+++ mail.php
@@ -1,13 +1,16 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST['name'])) {$name = $_POST['name'];}
-   // if (isset($_POST['phone'])) {$phone = $_POST['phone'];}
+  // if (isset($_POST['phone'])) {$phone = $_POST['phone'];}
+   if (isset($_POST['phone_full'])) {$code = $_POST['phone_full'];}
+   
     if (isset($_POST['formData'])) {$formData = $_POST['formData'];}
     if (isset($_POST['email'])) {$email = $_POST['email'];}
     if (isset($_POST['date'])) {$date = $_POST['date'];}
    // if (isset($_POST['budget'])) {$budget = $_POST['budget'];}
    
     if (isset($_POST['text'])) {$text = $_POST['text'];}
+   
 
     $to = "qwerty22625@gmail.com, energotr07@yandex.ru"; /*Укажите адрес, на который должно приходить письмо*/
     $sendfrom   = "$email"; /*Укажите адрес, с которого будет приходить письмо, можно не настоящий, нужно для формирования заголовка письма*/
@@ -16,7 +19,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $headers .= "MIME-Version: 1.0\r\n";
     $headers .= "Content-Type: text/html;charset=utf-8 \r\n";
     $subject = "$formData";
-    $message = "$formData<br> <b>Sender name:</b> $name <br><b>Email:</b> $email <br><b>Message:</b> $text" ;
+    $message = "$formData<br> <b>Sender name:</b> $name <br><b>Email:</b> $email <br><b>Phone:</b> $code  <br><b>Message:</b> $text " ;
     $send = mail ($to, $subject, $message, $headers);
     header('Location: /success.html'); //если header то переход на страницу success, и в ней верстается отображение сообщения, if тогда убрать
     /*if ($send == 'true')
