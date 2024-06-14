b98fe772ad75f72c7bed96c4cfbcfa5ee04a0ecd
Elenatr07 <e.turkina2010@yandex.ru>
2024-06-14 17:49:45 +1000

    10

--- scripts/animation.js
+++ scripts/animation.js
@@ -40,3 +40,28 @@
 
   observer.observe(square);
 })();
+
+document.addEventListener('DOMContentLoaded', function() {
+  let blocks = document.querySelectorAll('.block_item');
+
+  function checkBlocksVisibility() {
+      let windowHeight = window.innerHeight;
+
+      blocks.forEach(block => {
+          let blockPosition = block.getBoundingClientRect().top;
+          console.log(blockPosition)
+          if (blockPosition < windowHeight - 100) {
+              block.style.opacity = "1";
+              block.style.transform = "translateY(0)";
+          }
+      });
+  }
+
+  checkBlocksVisibility();
+
+  window.addEventListener('scroll', function() {
+      checkBlocksVisibility();
+  });
+
+
+});
